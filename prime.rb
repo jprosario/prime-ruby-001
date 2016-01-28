23f7e0fcef8d09c03efcdabea540c2d3bafc2c13
jprosario <jomi0388@gmail.com>
2016-01-28 16:45:19 -0500

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
-# Add  code here!
+def prime?(int)
+  divisors = 0
+  for i in (1..int)
+    if int % i == 0
+      divisors += 1
+    end
+  end
+  if divisors == 2
+    return true
+  else
+    return false
+  end
+end
